36b4f4a17f995f2e3c8eb259ece2ca43041a5366
dev-chester <chester.arellano@apptech-experts.com>
2020-04-14 01:04:17 +0800

    [adj] done with draft

--- index2.js
+++ index2.js
@@ -83,6 +83,10 @@ let postOptionREV = {
     'body': ""
 }
 
+let deleteOptionDraftBFI = {
+
+}
+
 const loginBFI = new Promise((resolve, reject) => {
     request(loginOptionBFI, (logerror, logresponse) => {
         if (logerror) reject(logerror);
@@ -130,15 +134,10 @@ let postBFIDraft = async function () {
     });
 }
 
-let removeDraft = async function (sPostedDraft, cookie) {
+let removeDraft = async function (removeDraftOption) {
     return new Promise((resolve, reject) => {
-        let removeDraftOption = {};
-        removeDraftOption.method = "DELETE";
-        removeDraftOption.url = `${process.env.SL_BASE_URL}/Drafts(${sPostedDraft})`;
-        removeDraftOption.headers["Content-Type"] = "application/json";
-        removeDraftOption.headers.Cookie = cookie;
         request(removeDraftOption, (err, res) => {
-            if (err) reject({
+            if (err) resolve({
                 "errorCode": "-1001",
                 "err": JSON.stringify(err)
             });
@@ -147,29 +146,33 @@ let removeDraft = async function (sPostedDraft, cookie) {
     });
 }
 
-let addDraft = async function (sPostedDraft, cookie) {
-    return new Promise((resolve, reject)=> {
-        let addDraftOption = {};
-        addDraftOption.method = "POST";
-        addDraftOption.url = `${process.env.SL_BASE_URL}/DraftsService_SaveDraftToDocument`;
-        addDraftOption.headers["Content-Type"] = "application/json";
-        removeDraftOption.headers.Cookie = cookie;
-        request(removeDraftOption, (err, res) => {
-            if (err) reject({
+let addDraft = async function (addDraftOption) {
+    return new Promise((resolve, reject) => {
+        request(addDraftOption, (err, res) => {
+            if (err) resolve({
                 "errorCode": "-1002",
                 "err": JSON.stringify(err)
             });
+
+            if (res.body.error) {
+                resolve({
+                    "errorCode": "-1003",
+                    "err": JSON.stringify(res.body)
+                });
+            }
             resolve(res);
-        })
+        });
 
     });
 }
 
 let postBFI = async () => {
     return new Promise((resolve, reject) => {
-        //resolve("asdf");
         request(postOptionBFI, (errpost, resppost) => {
-            if (errpost) reject("error1"); //return setTimeout(() => reject("Error : Request to POST in Engine Script BFI Purchase Order"), 500);
+            if (errpost) reject({
+                error: "-1005",
+                errorDesc: JSON.stringify(errpost)
+            }); //return setTimeout(() => reject("Error : Request to POST in Engine Script BFI Purchase Order"), 500);
             if (JSON.parse(resppost.body).error) {
                 reject(JSON.parse(resppost.body).error); //return setTimeout(() => reject(`SAP Error on Posting BFI PO from REV PO DocEntry ${docEntry}: \t${JSON.parse(resppost.body).error.message.value}  `), 500);
                 //throw new Error(JSON.parse(resppost.body).error.message.value);
@@ -184,11 +187,17 @@ let postREV = async () => {
     return new Promise((resolve, reject) => {
 
         request(postOptionREV, (errpost, resppost) => {
-            if (errpost) resolve({error: "-1003", errorDesc: errpost});
+            if (errpost) resolve({
+                error: "-1003",
+                errorDesc: errpost
+            });
             if (JSON.parse(resppost.body).error) {
-                resolve({error: "-1004", errorDesc: JSON.parse(resppost.body).error});
+                resolve({
+                    error: "-1004",
+                    errorDesc: JSON.parse(resppost.body).error
+                });
             }
-            resolve(resppost); 
+            resolve(resppost);
         });
     });
 }
@@ -238,7 +247,7 @@ async function start() {
                 //----PURCHASE ORDER DRAFT
                 bodyPurchaseOrder.DocDueDate = JSON.parse(poDetail)[0].DocDueDate;
                 bodyPurchaseOrder.CardCode = process.env.PO_CARDCODE;
-                bodyPurchaseOrder.Comments = `Based on REV Purchase Order DocEntry : ${JSON.parse(poDetail)[0].DocEntry} | DocNum : ${JSON.parse(poDetail)[0].DocNum}`;
+                bodyPurchaseOrder.Comments = `CPA TEST`; //`Based on REV Purchase Order DocEntry : ${JSON.parse(poDetail)[0].DocEntry} | DocNum : ${JSON.parse(poDetail)[0].DocNum}`;
                 bodyPurchaseOrder.NumAtCard = JSON.parse(poDetail)[0].NumAtCard;
                 bodyPurchaseOrder.U_APP_IsDBTran = "1";
                 bodyPurchaseOrder.DocObjectCode = "22";
@@ -257,14 +266,36 @@ async function start() {
 
             }
 
-            await startRowLoop(); 
-            let  postBFIres = await postBFI();
-            sPostedDraftDocEntry = JSON.parse(res.body).DocEntry;
+            await startRowLoop();
+            let postBFIres = await postBFI();
+            sPostedDraftDocEntry = JSON.parse(postBFIres.body).DocEntry;
             let postREVres = await postREV();
-            
-            if (JSON.parse(postREVres).error){
-                //let deleteDraftBFIres = await removeDraft(sPostedDraftDocEntry, bfiCookie);
-                //console.log(deleteDraftBFIres);
+
+            if (postREVres.error) {
+                let removeDraftOption = {};
+                removeDraftOption.method = "DELETE";
+                removeDraftOption.url = `${process.env.SL_BASE_URL}/Drafts(${sPostedDraftDocEntry})`;
+                removeDraftOption.headers = {
+                    "Cookie": bfiCookie
+                };
+                console.log(removeDraftOption);
+                let deleteDraftBFIres = await removeDraft(JSON.parse(JSON.stringify(removeDraftOption)));
+                console.log(deleteDraftBFIres);
+            } else {
+                let addDraftOption = {};
+                addDraftOption.method = "POST";
+                addDraftOption.url = `${process.env.SL_BASE_URL}/DraftsService_SaveDraftToDocument`;
+                addDraftOption.headers = {
+                    "Cookie" : bfiCookie
+                }
+                addDraftOption.body = JSON.stringify({
+                    "Document": {
+                        "DocEntry": sPostedDraftDocEntry
+                    }
+                });
+                let addActualDraftBFIres = await addDraft(JSON.parse(JSON.stringify(addDraftOption)));
+                console.log(addActualDraftBFIres);
+
             }
 
             // await postBFI()
@@ -273,7 +304,7 @@ async function start() {
             //         console.log(chalk.green(`BFI Purchase Order Draft Number : ${JSON.parse(res.body).DocEntry}`));
 
             //         await postREV();
-                        
+
 
             //     }).then((res) => {
             //         console.log(res);
